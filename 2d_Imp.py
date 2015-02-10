4e5d169272a4cd732716d5dec720b692405b9772
H. Aziz KAYIHAN <StZiza@users.noreply.github.com>
2015-02-10 19:17:33 +0200

    Update 2d_Imp.py
    Recent updates

--- 2d_Imp.py
+++ 2d_Imp.py
@@ -1,21 +1,19 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import pylab as pl
-from math import copysign
+from math import copysign, cos, sin, tan, radians
 from time import time
-
-#############################################
-#                                           #      
-#   PBC Two dimension implemention 1.1      #
-#                                           #
-#############################################
+import sys
 
 #I have an area of 100 to 100 in size from (-50,-50) to (50,50).
 # I will define a pencil beam of given distance.
 
 def data_point(box_size): #I am defining 50 random points x_data is x values and y_data is y.
     np.random.seed(250)
-    x_data = np.random.uniform(-1, 1, 50)*box_size*0.5
-    y_data = np.random.uniform(-1, 1, 50)*box_size*0.5
+    x_data = np.random.uniform(-1, 1, 50)*box_size*0.5 # x coordinates of the data
+    y_data = np.random.uniform(-1, 1, 50)*box_size*0.5 # y coordinates of the data
+#    print x_data #printing the data to check whether it works
+#    print y_data #printing the data to check whether it works
     return x_data, y_data
 
 
@@ -24,66 +22,54 @@ def data_point(box_size): #I am defining 50 random points x_data is x values and
 # Then I calculate the other bottom point and top points. The four coordinates of the pencil beams
 # Thus become (PB_six,-50), (PB_eix,-50), (PB_sfx,50) and (PB_efx,50). 
 
-def pencil_beam(box_size, pbs): # pbs is the length of the pencil beam.
+
+# by using while below, instead of for, it seems I might have solved the
+# tangent value problems for values larger than around 0.9.
+def PBC(box_size, xval): # xval x coordinates of the pencil beam corners
+    while abs(xval) > box_size*0.5:
+        xval = xval - box_size
+    return xval
     
-    slope = 0.5 #the slope is the tangent value of the pencil beam.
+
+def pencil_beam(box_size, pbs): # pbs is the width of the pencil beam.
+    
+    angle = 30 #în degrees #float(sys.argv[1])
+    slope = tan(radians(angle))  #the slope is the tangent value of the pencil beam.
+    pbl = 110 # pencil beam length
+    pbl_xref = pbl*cos(radians(angle)) # x correspondent of pbl
+    times = int(pbl_xref/box_size)
+    left = pbl_xref % box_size
+    
+    PB_six = []
+    PB_sfx = []
+    PB_eix = []
+    PB_efx = [] 
     
     np.random.seed() #clearing the seed so I can generate random pencil beam.
-    PB_six = np.random.uniform(-1, 1)*box_size*0.5
-    PB_sfx = PB_six + slope*box_size
-    PB_eix = PB_six + pbs
-    PB_efx = PB_eix + slope*box_size
+    six = np.random.uniform(-1, 1)*box_size*0.5
+    eix = six + pbs
+    pb_check = six + slope*box_size
     
-    return PB_six, PB_sfx, PB_eix, PB_efx, slope #The four values are all x values.
-
+    if pb_check < eix:
+        print ("Pencil Beam intersects itself, try higher angle or narrow"
+            " Pencil Beam")
+            
+    PB_six.append(six)
+    PB_eix.append(PBC(box_size, eix))
     
-
-# Trying to implement the periodic boundry condition:
-# Since I define the starting point of the pencil beam within my box, 
-# it will always be in the box, so I should chekc PBC for the other ends of the beam
-# However, I had another approach for checking the data inside the pencil beam below
-# so this conditions below proved to be quite useless. See below in halo check. If
-# this is OK to do it in the way -I explained below- I amy very well remove that part.
-# the rest of the calculations here are for plotting, to see the edges of pb on graph.
-
-
-def PBC2(box_size, pbs): # PBC applied to the end point of pencil beam
-    
-    PB_six, PB_sfx, PB_eix, PB_efx, slope = pencil_beam(box_size, pbs)
-    
-    if abs(PB_eix) > box_size*0.5:
-        PB_eix = PB_eix - box_size
-        PB_sfx = PB_sfx - box_size
-        PB_efx = PB_efx - box_size
-        Y2 = (box_size*0.5 - PB_six)/slope - box_size*0.5
-        pl.plot([PB_six, 50], [-50, Y2], 'k-', lw=1)
-        Y3 = box_size*0.5 - (PB_sfx + box_size*0.5)/slope
-        pl.plot([-50, PB_sfx], [Y3, 50], 'k-', lw=1)
-        pl.plot([PB_eix, PB_efx], [-50, 50], 'k-', lw=1)
-    elif abs(PB_sfx) > box_size*0.5:
-        PB_sfx = PB_sfx - box_size
-        PB_efx = PB_efx - box_size
-        Y2 = (box_size*0.5 - PB_six)/slope - box_size*0.5
-        pl.plot([PB_six, 50], [-50, Y2], 'k-', lw=1)
-        Y3 = box_size*0.5 - (PB_sfx + box_size*0.5)/slope
-        pl.plot([-50, PB_sfx], [Y3, 50], 'k-', lw=1)
-        Y4 = (box_size*0.5 - PB_eix)/slope - box_size*0.5
-        pl.plot([PB_eix, 50], [-50, Y4], 'k-', lw=1)
-        Y5 = box_size*0.5 - (PB_efx + box_size*0.5)/slope
-        pl.plot([-50, PB_efx], [Y5, 50], 'k-', lw=1)
-    elif abs(PB_efx) > box_size*0.5:
-        PB_efx = PB_efx - box_size
-        pl.plot([PB_six, PB_sfx], [-50, 50], 'k-', lw=1)
-        Y4 = (box_size*0.5 - PB_eix)/slope - box_size*0.5
-        pl.plot([PB_eix, 50], [-50, Y4], 'k-', lw=1)
-        Y5 = box_size*0.5 - (PB_efx + box_size*0.5)/slope
-        pl.plot([-50, PB_efx], [Y5, 50], 'k-', lw=1)
-    else:
-        pl.plot([PB_six, PB_sfx], [-50, 50], 'k-', lw=1)
-        pl.plot([PB_eix, PB_efx], [-50, 50], 'k-', lw=1)
-    return PB_six, PB_sfx, PB_eix, PB_efx, slope
+    for i in range(times):
+        PB_sfx.append(PBC(box_size, PB_six[i] + slope*box_size))
+        PB_efx.append((box_size, PB_sfx[i] + pbs))
+        PB_six.append(PB_sfx[i])
+        PB_eix.append(PB_efx[i])
     
 
+    PB_sfx.append(PBC(box_size, PB_six[times] + slope*left))
+    PB_efx.append(PBC(box_size, PB_sfx[times] + pbs))
+        
+    
+    print PB_six, PB_sfx, PB_eix, PB_efx  #printing the results to check whether it works
+    return PB_six, PB_sfx, PB_eix, PB_efx, slope, times, left #The four values are all x values.
 
     
 # Within this part, I included another segment (PBC_ind) to apply PBC to pencil beam
@@ -98,52 +84,63 @@ def PBC2(box_size, pbs): # PBC applied to the end point of pencil beam
 def halo_check(box_size, pbs): #seeing if the data point is in the pencil beam or not
     
     x_data, y_data = data_point(box_size)
+    X1, X2, Y1, Y2, slope, times, left = pencil_beam(box_size, pbs)
     
-    X1, X2, Y1, Y2, slope = PBC2(box_size, pbs)
-    PB_x = X1, X2, Y1, Y2
-    PB_y = box_size*0.5*-1, box_size*0.5, box_size*0.5*-1, box_size*0.5
-    
+    if X2[0] > X1[0]:
+        xdiff = X2[0] - X1[0]
+    else:
+        xdiff = box_size - abs(X2[0] - X1[0])
+        
         
                 
     def PBC_ind(value):
     
         diff = value + box_size*0.5
-        X_point = X1 + slope*diff
+        X_point = []
+        X_point.append(PBC(box_size, X1[0] + slope*diff))
         
-        if abs(X_point) > box_size*0.5:
-            X_point = X_point - box_size
+        for i in range(times):
+            X_point.append(PBC(box_size, X_point[i] + xdiff))
+#        print "X_point is", X_point #activate this line to check values
+        
+        #yval = (X2[times] - X1[times])/slope - box_size*0.5
+        #if value < yval:
+        #    X_point.append(PBC(box_size, X_point[times-1] + xdiff))
     
         return X_point
 
     
 #checking if the distances are more than the half box_size for PBC.            
-    
-    dx = x_data.copy()
-    for i in range(len(dx)):
-        dx[i] = dx[i] - PBC_ind(y_data[i])
-    
-    for i in range(len(dx)):
-        if abs(dx[i]) > box_size*0.5:
-            dx[i] = dx[i] - copysign(box_size, dx[i])
+    dx = []
+    dx.append(list(x_data))
+    for i in range(times):
+        dx.append(list(x_data))
         
+    for i in range(len(dx)):
+        for j in range(len(dx[i])):     
+            dx[i][j] = dx[i][j] - PBC_ind(y_data[j])[i]
+            if abs(dx[i][j]) > box_size*0.5:
+                dx[i][j] = dx[i][j] - copysign(box_size, dx[i][j])  
+#    print dx #activate to check points
 
 #Here counting the halos in the pencil beam.    
             
     x_in = []
     y_in = []
     n = 0
-    for i in range(len(dx)):
-        if 0 <= dx[i] < pbs:
-            n += 1
-            x_in.append(x_data[i])
-            y_in.append(y_data[i]) 
+    for i in range(len(x_data)):
+        for j in range(len(dx)):
+            if 0 <= dx[j][i] < pbs:
+                n += 1
+                x_in.append(x_data[i])
+                y_in.append(y_data[i]) 
+#                print "particle", x_data[i], y_data[i], "is in the pencil beam"    #activate to check points
+#            else: #activate to check points
+#                print "particle", x_data[i], y_data[i], "is NOT in the pencil beam" #activate to check points
     
+#    print dx        #activate to check points
     print "there are", n, "particles in the pencil beam"
     
-    pl.scatter(x_data, y_data)
-    pl.scatter(PB_x, PB_y, c='r')
-    pl.scatter(x_in, y_in, c='w')
-    pl.show()
 
             
                                     
